8ecda811e2dbef6aaa33963598c4f2404db618a5
Antonis Vozikis <macbook@antoniss-macbook-pro.local>
2018-11-10 17:35:27 +0200

    Init a Tab Bar Controller with 3 Views

---
+++ TestingTabBar/ThirdViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ThirdViewController.swift
+//  TestingTabBar
+//
+//  Created by Antonis Vozikis on 10/11/2018.
+//  Copyright © 2018 Antonis Vozikis. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    @IBOutlet var initialView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initialView.backgroundColor = UIColor.red
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
