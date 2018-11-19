196b62f07138f6d7a0427e7bbf235b74118f5993
Korpel <Vozikisant@gmail.com>
2018-11-19 22:40:50 +0200

    pan gestures working. Moving monkey on third view controller

--- TestingTabBar/ThirdViewController.swift
+++ TestingTabBar/ThirdViewController.swift
@@ -14,11 +14,11 @@ class ThirdViewController: UIViewController {
     @IBAction func panHandler(_ recognizer: UIPanGestureRecognizer) {
         let translation = recognizer.translation(in: self.view)
         if let view = recognizer.view {
-            view.center = CGPoint(x:view.center.x + translation.x,
-                                  y:view.center.y + translation.y)
+            imageMonkey.center = CGPoint(x:imageMonkey.center.x + translation.x,
+                                  y:imageMonkey.center.y + translation.y)
         }
         recognizer.setTranslation(CGPoint.zero, in: self.view)
-        print(translation)
+
     }
     
     @IBOutlet var initialView: UIView!
