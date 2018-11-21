f195751bc6c8e4b0f3860ff90b298950ac3c0b1a
Korpel <Vozikisant@gmail.com>
2018-11-21 13:33:03 +0200

    Gave the new View Controller's monkey property movable actions

--- TestingTabBar/ViewController.swift
+++ TestingTabBar/ViewController.swift
@@ -34,6 +34,11 @@ class ViewController: UIViewController {
 
     @IBAction func HandleMonkeyPan(_ sender: UIPanGestureRecognizer) {
         let translation = sender.translation(in: self.view)
+        if let view = sender.view {
+            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+        }
+        sender.setTranslation(CGPoint.zero, in: self.view)
+        
     }
     
   override func viewDidLoad() {
