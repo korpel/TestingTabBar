569b002ab63db3b9f5bfd966d4d9e4c86a7d5b41
Korpel <Vozikisant@gmail.com>
2018-11-24 12:14:31 +0200

    a

--- TestingTabBar/LatestViewController.swift
+++ TestingTabBar/LatestViewController.swift
@@ -14,7 +14,7 @@ class LatestViewController: UIViewController, UITableViewDataSource, UITableView
     
         // Data model: These strings will be the data for the table view cells
         var animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
-        var refresh: UIRefreshControl?
+    
         
         // cell reuse id (cells that scroll out of view can be reused)
         let cellReuseIdentifier = "cell"
@@ -60,12 +60,7 @@ class LatestViewController: UIViewController, UITableViewDataSource, UITableView
             self.animals.append("my name")
             print(self.animals)
             self.tableView.setNeedsDisplay()
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-                self.refresh?.endRefreshing()
-                
-            }
-            
+            self.view.setNeedsDisplay()
         })
         return UISwipeActionsConfiguration(actions: [editAction])
     }
