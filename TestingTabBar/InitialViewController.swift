38ef6a3ddc75b39e6f27d7fbfbf4d080f7a7959b
Korpel <Vozikisant@gmail.com>
2018-12-06 19:54:22 +0200

    Restarting from scratch

--- TestingTabBar/InitialViewController.swift
+++ TestingTabBar/InitialViewController.swift
@@ -16,45 +16,4 @@ class InitialViewController: UIViewController {
         
     }
     
-    @IBAction func buttonTouched(_ sender: UIButton) {
-        let alert = UIAlertController(title: "just a title", message: "just a message", preferredStyle: .alert)
-        self.present(alert, animated: true, completion: nil)
-    }
-    @IBAction func secondButtonTouched(_ sender: UIButton) {
-        let alert = UIAlertController(title: "just a title", message: "just a message", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "first", style: .default, handler: nil))
-        alert.addAction(UIAlertAction(title: "second", style: .cancel, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    @IBAction func thirdButtonTouched(_ sender: UIButton) {
-        let alert = UIAlertController(title: "just a title", message: "just a message", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "first", style: .default, handler: nil))
-        alert.addAction(UIAlertAction(title: "second", style: .cancel, handler: nil))
-        alert.addAction(UIAlertAction(title: "third", style: .destructive, handler: nil))
-        alert.addAction(UIAlertAction(title: "fourth", style: .default, handler: {action in print("tony")}))
-        self.present(alert, animated: true, completion: nil)
-    }
-    @IBAction func fourthButtonTouched(_ sender: UIButton) {
-    }
-    @IBAction func fifthButtonTouched(_ sender: UIButton) {
-    }
-    @IBAction func sixthButtonTouched(_ sender: UIButton) {
-        let alert = UIAlertController(title: "just a title", message: "just a message", preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "first", style: .default, handler: nil))
-        alert.addAction(UIAlertAction(title: "second", style: .cancel, handler: nil))
-        alert.addAction(UIAlertAction(title: "third", style: .destructive, handler: nil))
-        alert.addAction(UIAlertAction(title: "fourth", style: .default, handler: {action in print("tony")}))
-        self.present(alert, animated: true, completion: nil)
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
