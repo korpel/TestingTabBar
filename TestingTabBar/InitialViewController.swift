45db89afb7f333c67aa8160369c7db38d66a90c0
Korpel <Vozikisant@gmail.com>
2018-12-10 13:03:05 +0200

    Restarting

--- TestingTabBar/InitialViewController.swift
+++ TestingTabBar/InitialViewController.swift
@@ -9,35 +9,6 @@
 import UIKit
 
 class InitialViewController: UIViewController {
-    @IBAction func datechanged(_ sender: UIDatePicker) {
-        print(date.date)
-    }
-    @IBOutlet weak var date: UIDatePicker!
-    var counter = 0
-    @IBOutlet weak var someButton: UIButton!
     
-    @IBOutlet weak var someLabel: UILabel!
-    @IBOutlet weak var someControler: UISegmentedControl!
-    
-    @IBAction func someControlerTap(_ sender: UISegmentedControl) {
-        someLabel.text = "\(counter)"
-        counter += 1
-        if someControler.selectedSegmentIndex == 0 {
-            someButton.setTitle("first", for: .normal)
-        } else if someControler.selectedSegmentIndex == 1 {
-           someButton.setTitle("deftero", for: .normal)
-        }
-    }
-    @IBAction func someButtonTap(_ sender: UIButton) {
-        someControler.sendActions(for: .allEvents)
-        print(someControler.titleForSegment(at: 0))
-    }
-    
-    @IBOutlet var initialView: UIView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        someControler.sendActions(for: .allEvents)
-        
-    }
     
 }
